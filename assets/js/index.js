c6c71521679fb360180e44bdd3474914da1a89a6
Sebastian F <sebpfave2@gmail.com>
2021-03-14 14:05:18 -0400

    Fix table row creation and appending

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,20 @@
 // Render projects to table
 
 // Create project row to add to table project
-function addProjectToList() {
-  const projList = $("project-list");
+function addProjectToList(proj) {
+  const projList = $("#project-list");
 
+  // Create table row element, fill in text content, append to table body
   const projRow = $("<tr>");
-  projRow.innerHTML = `
+  projRow.html(`
     <td>${proj.name}</td>
     <td>${proj.type}</td>
     <td>${proj.rate}</td>
     <td>${proj.dueDate}</td>
     <td>${proj.numDaysDue}</td>
     <td>${proj.earning}</td>
-    <td><button class="btn btn-danger btn-sm remove">X</button></td>
-  `;
+    <td><button class="btn btn-danger btn-sm remove">&times;</button></td>
+  `);
   projList.append(projRow);
 }
 
@@ -32,3 +33,15 @@ $("project-list").on("click", (e) => {
   //
   removeProjectFromList(e.target);
 });
+
+// Webpage Execution
+const project = {
+  name: "project1",
+  type: "web dev",
+  rate: "120",
+  dueDate: "8/1/21",
+  numDaysDue: "100",
+  earning: "1200",
+};
+
+addProjectToList(project);
